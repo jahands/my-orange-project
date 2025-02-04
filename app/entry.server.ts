7dd2fd29c9b09c51b6f24997f5abe85eb093090d
Jacob Hands <jacob@gogit.io>
2025-02-04 13:21:17 -0600

    Add hono app

--- app/entry.server.ts
+++ app/entry.server.ts
@@ -1,6 +1,18 @@
-import { app } from "@orange-js/orange/server";
-import * as serverBuild from "virtual:orange/server-bundle";
+import { app } from "@orange-js/orange/server"
+import { Hono } from "hono"
+import * as serverBuild from "virtual:orange/server-bundle"
 
-export * from "virtual:orange/entrypoints";
+export * from "virtual:orange/entrypoints"
 
-export default app(serverBuild);
+export const honoApp = new Hono<{ Bindings: Env }>()
+
+  // API routes that bypass the react app
+  .get("/api/ping", (c) => c.text("pong"))
+
+  // plain 404 response for all invalid api routes
+  .all("/api/*", (c) => c.notFound())
+
+  // Send everything else to the react app
+  .mount("/", app(serverBuild).fetch)
+
+export default honoApp
